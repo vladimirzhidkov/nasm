184f1cbe9d9087aa0439c98c449dc711845bbe13
vzhidkov <vladimir.zhidkov@icloud.com>
2023-11-15 10:18:45 -0500

    3.11

--- 3_11/start.asm
+++ 3_11/start.asm
@@ -1,10 +1,6 @@
 section .bss
 buf:		resb	512
 
-section .data
-ok:		db	"OK", 0xa
-len_ok		equ	$ - ok
-
 section .text
 global _start
 _start:
@@ -19,13 +15,41 @@ read:
 		; check for EOF
 		test	eax, eax
 		jz	exit	
+	
+		; count sum of all digits
+		xor	edx, edx	; sum
+		mov	ecx, eax	; loop counter
+		xor	eax, eax
+		mov	esi, buf
+		cld
+lp1:
+		lodsb			; [esi]->al; inc esi
+	if:	cmp	al, '1'
+		jb	endif
+		cmp	al, '9'
+		ja	endif
+		sub	al, '0'		; get actual digit 
+		add	edx, eax
+	endif:
+		loop	lp1
+lp1_done:
+		; form string of * with length of EBX
+		mov	ecx, edx
+		jecxz	exit
+		mov	edi, buf
+		cld
+		mov	al, '*'
+lp2:
+		stosb			; al->[edi]; inc edi
+		loop	lp2
+		mov	byte[edi], 0xa	; add newline character
+		inc	edx	
 print:
 		mov	eax, 4		; sys_write
 		mov	ebx, 1		; stdout
-		mov	ecx, ok
-		mov	edx, len_ok
+		mov	ecx, buf 
+		; edx was set above
 		int	0x80
-		jmp	read
 exit:
 		mov	eax, 1		; sys_exit
 		mov	ebx, 0		; return status
