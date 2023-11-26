8bf713cb5ce869d7cf1ddda7c8e55c06cfc6cf75
vzhidkov <vladimir.zhidkov@icloud.com>
2023-11-25 22:18:59 -0500

    modified 3.08

--- 3_07/start.asm
+++ 3_07/start.asm
@@ -1,43 +1,41 @@
 section .bss
-buffer:	resb 2		; reserve 1 byte for a letter, and the second one for press enter
+buf:		resb	1
 
-section .data
-yes	db 'YES', 0xA	; followed by newline
-len_yes	equ $ - yes	; length
-
-no	db 'NO', 0xA
-len_no	equ $ - no	
-
+section .rodata
+YES		db	'YES', 0xA	
+LEN_YES		equ	$ - YES	
+NO		db	'NO', 0xA
+LEN_NO		equ	$ - NO	
 
 section .text
 global _start
-_start:
-		; read from stdin
+_start:		; read from stdin
 		mov	eax, 3		; sys_read
 		mov	ebx, 0		; stdin file descriptor
-		mov	ecx, buffer
-		mov	edx, 2		; number of bytes to read
+		mov	ecx, buf
+		mov	edx, 1		; number of bytes to read
 		int	0x80
-
-		; check if buffer[0] is 'A'
-		mov	al, [buffer]
+		; check for EOF
+		test	eax, eax
+		jz	exit
+		; check buf
+		mov	al, [buf]
 		cmp	al, 'A' 
-		jnz	print_no
-
-print_yes:
-		mov	eax, 4		; sys_write
+		je	print_yes	; al = A
+		cmp	al, 0xa
+		je	_start		; al = newline so ignore it	
+print_no:	mov	eax, 4		; sys_write
 		mov	ebx, 1  	; stdout
-		mov	ecx, yes
-		mov	edx, len_yes	; length
+		mov	ecx, NO 
+		mov	edx, LEN_NO	; length
 		int	0x80
-		jmp	exit
-print_no:
-		mov	eax, 4		; sys_write
+		jmp	_start
+print_yes:	 mov	eax, 4		; sys_write
 		mov	ebx, 1  	; stdout
-		mov	ecx, no 
-		mov	edx, len_no	; length
+		mov	ecx, YES
+		mov	edx, LEN_YES	; length
 		int	0x80
-exit:
-		mov	ebx, 0	; return status
+		jmp	_start
+exit:		mov	ebx, 0	; return status
 		mov	eax, 1	; sys_exit
 		int	0x80	; call kernel 
--- 3_08/start.asm
+++ 3_08/start.asm
@@ -1,46 +1,74 @@
 section .bss
-buf:		resb	2
-str:		resb	10
+LEN_BUF:	equ	3
+buf:		resb	LEN_BUF
+LEN_STR:	equ	10
+str:		resb	LEN_STR
+
+section .data
+error:		dd	0
+digit:		db	0, 0xa
+
+section .rodata
+MSG_ERROR:	db	"Input error", 0xa
+LEN_MSG_ERROR:	equ	$ - MSG_ERROR
 
 section .text
 global _start
-_start:
-		; read number
+_start:		; read input
 		mov	eax, 3		; sys_read
 		mov	ebx, 0		; stdin	
 		mov	ecx, buf
-		mov	edx, 2		; number of bytes to read
-		int	0x80		; call kernel
-
-		; check if it is [1-9]
-		xor	ecx, ecx
-		mov	cl, [buf]	; get input character
-		cmp	cl, '1'		
-		jb	exit		; < 1
-		cmp	cl, '9'		
-		ja	exit		; > 9
-
-		; convert to number
-		sub	cl, '0'
-
-		; store number for later use in sys_write
-		mov	edx, ecx	
-
-		; create a string of '*' 
-		mov	edi, str
+		mov	edx, LEN_BUF
+		int	0x80
+		; check for EOF
+		test	eax, eax
+		jz	exit	
+		; iterate through buf
+		xor	ebx, ebx	; char
+		mov	esi, buf
+read_char:	mov	bl, [esi]
+		cmp	bl, 0xa		; NL
+		jz	end_input	; bl = NL 
+		cmp	dword[error], 0
+		jnz	next		; there was input error earlier
+		cmp	byte[digit], 0
+		jnz	set_error	; there was already digit
+		cmp	bl, '0'
+		jb	set_error
+		cmp	bl, '9'
+		ja	set_error
+		mov	[digit], bl
+		jmp	next
+set_error:	mov	dword[error], MSG_ERROR 
+next:		inc	esi
+		dec	eax
+		jnz	read_char
+		jmp	_start
+end_input:	cmp	byte[digit], 0
+		jnz	print		
+		mov	dword[error], MSG_ERROR	
+print:		mov	ecx, [error]
+		test	ecx, ecx
+		jz	form_str	; there was no input errors
+		mov	edx, LEN_MSG_ERROR
+		jmp	sys_write
+form_str:	xor	ecx, ecx	
+		mov	cl, [digit]
+		sub	ecx, '0'
+		mov	edx, ecx
 		mov	al, '*'
+		mov	edi, str
 		cld
-lp:
-		stosb		; AL->[EDI]; inc EDI
-		loop	lp
-		mov	byte[edi], 0xa	; add newline to the end of str
+		rep stosb
+		mov	byte[edi], 0xa
 		inc	edx
-print:
-		mov	eax, 4		; sys_write
+		mov	ecx, str 
+sys_write:	mov	eax, 4		; sys_write
 		mov	ebx, 1		; stdout
-		mov	ecx, str	; string to print 
-		int	0x80		; call kernel 	
-exit:
-		mov	eax, 1	; sys_exit
+		int	0x80
+reset:		mov	byte[digit], 0
+		mov	dword[error], 0	
+		jmp	_start	
+exit:		mov	eax, 1	; sys_exit
 		mov	ebx, 0	; return status
-		int	0x80	; call kernel
+		int	0x80	
