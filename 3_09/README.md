fca73f0994e1290b80de2bdb2cb2afb4243b323f
vzhidkov <vladimir.zhidkov@icloud.com>
2023-11-26 17:22:27 -0500

    changes to 3.09

---
+++ 3_09/README.md
@@ -0,0 +1,2 @@
+# Exercise 3.09
+Write a program that reads data from the standard input stream and immediately outputs it to the standard output stream until the 'end-of-file' condition is reached (i.e., it does the same thing that the cat utility does when run without arguments).
--- 3_09/start.asm
+++ 3_09/start.asm
@@ -1,28 +1,24 @@
 section .bss
-buf:		resb	512
+LEN_BUF:	equ	5
+buf:		resb	LEN_BUF	
 
 section .text
 global _start
-_start:
-
-read:
-		mov	eax, 3		; sys_read
+_start:		mov	eax, 3		; sys_read
 		mov	ebx, 0		; stdin
 		mov	ecx, buf
-		mov	edx, 512	; num of characters
-		int	0x80		; call kernel
-; check for EOF
-		test	eax, eax	; eax holds return value from sys_read
+		mov	edx, LEN_BUF 
+		int	0x80
+		; check for EOF
+		test	eax, eax
 		jz	exit
-write:
+		; write to stdout
 		mov	edx, eax	; num of characters
 		mov	eax, 4		; sys_write
 		mov	ebx, 1		; stdout
 		mov	ecx, buf
-		int	0x80		; call kernel
-; go back to reading
-		jmp	read
-exit:
-		mov	ebx, eax	; return status
+		int	0x80		
+		jmp	_start	
+exit:		mov	ebx, eax	; return status
 		mov	eax, 1		; sys_exit
-		int	0x80		; call kernel
+		int	0x80	
