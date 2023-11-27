a410c5731e9eee7d33e303c5284aee43d70b7fcf
vzhidkov <vladimir.zhidkov@icloud.com>
2023-11-27 13:25:19 -0500

    changed 3.10

---
+++ 3_10/README.md
@@ -0,0 +1,2 @@
+# Exercise 3.10
+Write a program that reads characters from the standard input stream until the end of the line (i.e., until a character with code 10 is encountered or an 'end-of-file' situation arises) and counts how many '+' and '-' characters are encountered in this line; all other characters are ignored. Let's call the resulting counts N+ and N-. After the end of the line's input, the program calculates the number N* = N+ x N- and prints that number of asterisks.
--- 3_10/start.asm
+++ 3_10/start.asm
@@ -1,66 +1,71 @@
 section .bss
-buf:		resb	512
+LEN_BUF:	equ	2		; any > 1	
+buf:		resb	LEN_BUF		
+
+section .data
+plus_count:	db	0
+minus_count:	db	0
 
 section .text
 global _start
-_start:
-
-read:		mov	eax, 3		; sys_read
+_start:		mov	byte[plus_count], 0
+		mov	byte[minus_count], 0	
+read_stdin:	; read input
+		mov	eax, 3		; sys_read
 		mov	ebx, 0		; stdin
 		mov	ecx, buf
-		mov	edx, 512
+		mov	edx, LEN_BUF 
 		int	0x80
+		; check for EOF
 		test	eax, eax	; holds number or characters or 0 for EOF
 		jz	exit
-
-		; count +/- symbols
-		mov	ecx, eax	; loop counter
+		; iterate through buf
+		; eax = input length
 		mov	esi, buf
-		cld
-		xor	ebx, ebx	; counter for '+'
-		xor	edx, edx	; counter for '-'
-lp1:
-		lodsb
-if1:		
-		cmp	al, '+'
-		jnz	if2	
-		inc	ebx		
-		jmp	endif
-if2:	
-		cmp	al, '-'
-		jnz	endif
+read_nxt_char:	movzx	ebx, byte[esi]
+		cmp	ebx, 0xa
+		je	calc_prod	; EBX = NL character	
+		cmp	ebx, '+'
+		je	inc_plus_count	; EBX = '+'
+		cmp	ebx, '-'
+		je	inc_minus_count	; EBX = '-'
+		jmp	nxt_iter
+inc_plus_count:	inc	byte[plus_count]
+		jmp	nxt_iter
+inc_minus_count:inc	byte[minus_count]
+nxt_iter:	inc	esi
+		dec	eax
+		jnz	read_nxt_char
+		jmp	read_stdin	
+calc_prod:	mov	al, [plus_count]
+		mul	byte[minus_count]	; product->AX = total length
+		; split total length into series of bufs
+nxt_buf:	cmp	eax, LEN_BUF-1	; reserve 1 spot for NL char
+		jbe	last_buf	
+		sub	eax, LEN_BUF-1	; reduce total length left (reserve 1 spot for NL char)
+		mov	ecx, LEN_BUF-1	; char counter for stosb use (reserve 1 spot for NL char) 
+		mov	edx, ecx	; char counter for sys_write use
+		jmp	fill_buf	
+last_buf:	mov	ecx, eax	; char counter for stosb use 
+		xor	eax, eax	; clear total length left
+		mov	byte[buf+ecx], 0xa	; add NL character
+		mov	edx, ecx	; char counter for sys_write use
 		inc	edx
-endif:	
-		loop	lp1		
-multiply:
-		mov	eax, ebx	
-		mul	dl
-		
-		; store result of multiplication for later use
-		mov	edx, eax
-
-		; check for zero result	
-		test	edx, edx
-		jz	exit		
-		
-		; form string of * with length of eax
-		mov	ecx, eax	; loop counter
+fill_buf:	push	eax		; save total length left
+		; form string
+		mov	al, '*'	
 		mov	edi, buf
 		cld
-		mov	al, '*'
-lp2:
-		stosb			; al->[edi]; inc edi
-		loop	lp2
-		mov	byte[edi], 0xa	; add newline	
-		inc	edx
-print:
-		mov	eax, 4	; sys_write
-		mov	ebx, 1	; stdout
+		rep stosb 
+		; write to stdout
+		mov	eax, 4		; sys_write 
+		mov	ebx, 1		; stdout
 		mov	ecx, buf
-		; edx was set above after multiplication
 		int	0x80 
-
-exit:
-		mov	eax, 1	; sys_exit
-		mov	ebx, 0	; return status
-		int	0x80	; call kernel
+		pop	eax		; restore total length left
+		test	eax, eax
+		jnz	nxt_buf
+		jmp	_start
+exit:		mov	eax, 1		; sys_exit
+		mov	ebx, 0		; return status
+		int	0x80
