261ffef2c3066134991ed2393532a7eb4a7a6530
vzhidkov <vladimir.zhidkov@icloud.com>
2023-11-27 15:00:29 -0500

    changed 3.11

---
+++ 3_11/README.md
@@ -0,0 +1,2 @@
+# Exercise 3.11
+Write a program that reads characters from the standard input stream until the end of the line (i.e., until a character with code 10 is encountered or an 'end-of-file' situation arises) and calculates the sum of all encountered digits. At the end of the line's input, the program should print a number of asterisks equal to the calculated sum.
--- 3_11/start.asm
+++ 3_11/start.asm
@@ -1,56 +1,59 @@
 section .bss
-buf:		resb	512
+LEN_BUF:	equ	2		; any >  1
+buf:		resb	LEN_BUF		; for both stdin and stdout
+
+section .data
+sum:		dd	0
 
 section .text
 global _start
-_start:
-
-read:
-		mov	eax, 3		; sys_read
+_start:		mov	eax, 3		; sys_read
 		mov	ebx, 0		; stdin
 		mov	ecx, buf
-		mov	edx, 512
+		mov	edx, LEN_BUF 
 		int	0x80
-		
 		; check for EOF
 		test	eax, eax
 		jz	exit	
-	
-		; count sum of all digits
-		xor	edx, edx	; sum
-		mov	ecx, eax	; loop counter
-		xor	eax, eax
+		; iterate through buf
 		mov	esi, buf
-		cld
-lp1:
-		lodsb			; [esi]->al; inc esi
-	if:	cmp	al, '1'
-		jb	endif
-		cmp	al, '9'
-		ja	endif
-		sub	al, '0'		; get actual digit 
-		add	edx, eax
-	endif:
-		loop	lp1
-lp1_done:
-		; form string of * with length of EBX
-		mov	ecx, edx
-		jecxz	exit
-		mov	edi, buf
-		cld
+		xor	ebx, ebx	; input character
+read_nxt_char:	mov	bl, [esi]	
+		cmp	bl, 0xa
+		jz	prnt_nxt_buf	; BL = NL character	
+		cmp	bl, '0'
+		jb	nxt_iter	; BL is not a digit character, so ignore it
+		cmp	bl, '9'
+		ja	nxt_iter	; BL is not a digit character, so ignore it
+		sub	bl, '0'		; convert BL to number
+		add	dword[sum], ebx
+nxt_iter:	inc	esi
+		dec	eax
+		jnz	read_nxt_char
+		jmp	_start	
+prnt_nxt_buf:	mov	eax, [sum]	; total length left
+		test	eax, eax
+		jz	_start
+		cmp	eax, LEN_BUF-1	; -1 is for reserving spot for NL character 
+		jbe	last_buf	; 	
+		sub	eax, LEN_BUF-1	; reduce total length left by buf length - 1
+		mov	ecx, LEN_BUF-1	; for stosb use
+		mov	dword[sum], eax ; updated length left
+		mov	edx, ecx	; for sys_write
+		jmp	form_str
+last_buf:	mov	ecx, eax	; for stosb use
+		mov	byte[buf+ecx], 0xa	; add NL char to the end
+		mov	edx, ecx	; for sys_write
+		inc	edx
+		mov	dword[sum], 0	
+form_str:	mov	edi, buf
 		mov	al, '*'
-lp2:
-		stosb			; al->[edi]; inc edi
-		loop	lp2
-		mov	byte[edi], 0xa	; add newline character
-		inc	edx	
-print:
+		rep stosb
 		mov	eax, 4		; sys_write
 		mov	ebx, 1		; stdout
-		mov	ecx, buf 
-		; edx was set above
+		mov	ecx, buf	
 		int	0x80
-exit:
-		mov	eax, 1		; sys_exit
+		jnz	prnt_nxt_buf	
+exit:		mov	eax, 1		; sys_exit
 		mov	ebx, 0		; return status
 		int	0x80
