d7ee92dbc573eb511e829977ba56aeaa392af19c
vzhidkov <vladimir.zhidkov@icloud.com>
2023-11-28 10:26:34 -0500

    refactoring

--- 3_07/start.asm
+++ 3_07/start.asm
@@ -2,9 +2,6 @@ section .bss
 LEN_BUF:	equ	4		; any > 0
 buf:		resb	LEN_BUF		; stdin buffer
 
-section .data
-msg:		dd	0
-
 section .rodata
 YES:		db	'YES', 0xA	
 NO:		db	'NO ', 0xA
@@ -12,7 +9,8 @@ LEN_MSG:	equ	4		; 3 for YES/NO, 1 for NL character
 
 section .text
 global _start
-_start:		mov	eax, 3		; sys_read
+_start:		xor	edi, edi	; message pointer to YES/NO, if 0 then fresh read
+read_stdin:	mov	eax, 3		; sys_read
 		mov	ebx, 0		; stdin file descriptor
 		mov	ecx, buf
 		mov	edx, LEN_BUF	; number of bytes to read
@@ -20,29 +18,28 @@ _start:		mov	eax, 3		; sys_read
 		test	eax, eax	; eax = input length
 		jz	exit		; EOF is reached 
 		mov	esi, buf	; iterate through buf
-read_nxt_char:	movzx	ebx, byte[esi]	; BL is input character
+read_nxt_char:	mov	bl, byte[esi]	; BL is input character
 		cmp	bl, 0xa
 		je	print		; BL = NL character 	
-		cmp	dword[msg], 0	
+		cmp	edi, 0		; check msg pointer
 		jne	set_msg_no	; there was already input processed, so set NO
 		cmp	bl, 'A' 
 		jne	set_msg_no	; BL != 'A'
-		mov	dword[msg], YES 
+		mov	edi, YES
 		jmp	next_iter
-set_msg_no:	mov	dword[msg], NO 
+set_msg_no:	mov	edi, NO	
 next_iter:	inc	esi
 		dec	eax
 		jnz	read_nxt_char
-		jmp	_start	
-print:		mov	ecx, [msg]	
-		test	ecx, ecx
+		jmp	read_stdin	
+print:		test	edi, edi
 		jnz	sys_write	; msg is not empty, so send it to stdout
 		mov	ecx, NO		; msg was empty
 sys_write:	mov	eax, 4		; sys_write
 		mov	ebx, 1  	; stdout
+		mov	ecx, edi
 		mov	edx, LEN_MSG
 		int	0x80
-		mov	dword[msg], 0	; reset to  default msg 
 		jmp	_start	
 exit:		mov	eax, 1		; sys_exit
 		mov	ebx, 0		; return status
