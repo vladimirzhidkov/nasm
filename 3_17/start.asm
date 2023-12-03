4515e4ac767665a3063161af0d1a3c04af781c77
vzhidkov <vladimir.zhidkov@icloud.com>
2023-12-03 16:19:48 -0500

    experimenting

--- 3_17/start.asm
+++ 3_17/start.asm
@@ -1,68 +1,78 @@
 section .bss
-in:		resb	256
-out:		resb	512
+LEN_INPUT_BUF:	equ	8		; any > 0
+input_buf:	resb	LEN_INPUT_BUF
+LEN_OUTPUT_BUF:	equ	8
+output_buf:	resb	LEN_OUTPUT_BUF
+
+section .data
+len_word:	dd	0		; current word length
 
 section .text
 global _start
-_start:
-		; read from stdin
+_start:		mov	edi, output_buf
+		mov	ecx, LEN_OUTPUT_BUF
+read_stdin:	push	ecx
 		mov	eax, 3		; sys_read
 		mov	ebx, 0		; stdin
-		mov	ecx, in 
-		mov	edx, 512
+		mov	ecx, input_buf 
+		mov	edx, LEN_INPUT_BUF 
 		int	0x80
-		
-		; check for EOF
-		test	eax, eax
+		pop	ecx
+		test	eax, eax	; check for EOF
 		jz	exit
-
-		; iterate through buf
-		; al is current character
-		; ah is 0 if outside word
-		;	1 if inside word 
-		; ecx is loop counter
-		mov	ecx, eax
-		xor	eax, eax
-		mov	esi, in 
-		mov	edi, out 
-lp1:		lodsb
-		; if al == ' ' || al == '\n'
-		;	check if prev position (ah != 0) was inside word
-		;else
-		;	check if prev position (ah == 0) was outside word
-		cmp	al, ' '
-		jnz	or
-		; current character (al) is ' ' or '\n'
-chk_ah1:	; check if prev position was inside word (ah != 0) 
-		test	ah, ah
-		jz	cpy
-		mov	[edi], byte ')'
-		inc	edi
-		xor	ah, ah
-		jmp	cpy	
-or:		cmp	al, 0xa		; newline character
-		jz	chk_ah1
-else:		; current character (al) is not ' ' or '\n'	
-		; check if prev position  was outside word (ah == 0)
-		test	ah, ah
-		jnz	cpy	
-		mov	[edi], byte '('
-		inc	edi
-		mov	ah, 1
-cpy:		; copy al->out
-		mov	[edi], al
-		inc	edi
-		loop lp1 
-		
-		; print from out
-		mov	eax, 4		; sys_write
-		mov	ebx, 1		; stdout
-		mov	ecx, out
-		mov	edx, edi
-		sub	edx, out
-		int	0x80
-		jmp	_start
-exit:
-		mov	eax, 1		; sys_exit
+		; iterate through input_buf
+		mov	esi, input_buf
+read_next_char:	mov	dl, [esi]
+		cmp	dl, 0xa		; check for NL	
+		jnz	check_blank
+		jmp	check_len_word
+check_blank:	cmp	dl, ' '
+		jnz	check_new_word
+check_len_word:	cmp	dword[len_word], 0
+		jz	print_char
+		mov	dword[len_word], 0
+		mov	byte[edi], ')' 
+		call	print		
+		jmp	print_char	
+check_new_word:	cmp	dword[len_word], 0
+		jnz	inc_len_word
+		mov	byte[edi], '(' 
+		call	print
+inc_len_word:	inc	dword[len_word]
+print_char:	mov	byte[edi], dl	; print current char
+		call	print	
+next_iter:	cmp	dl, 0xa		; check for NL	
+		jz	flush_output_buf
+		inc	esi
+		dec	eax
+		jnz	read_next_char	; still have chars left in input_buf
+		jmp	read_stdin	; input_buf is empty, so jump to fill it up again with chars from stdin
+exit:		mov	eax, 1		; sys_exit
 		mov	ebx, 0		; return status
 		int	0x80 
+flush_output_buf:
+		mov	eax, 4
+		mov	ebx, 1
+		mov	ecx, output_buf
+		mov	edx, edi
+		sub	edx, output_buf
+		int	0x80
+		jmp	_start	
+; prints to outpub buf. if full, flush it to stdout
+print:		inc	edi	
+		dec	ecx
+		jnz	.done	
+.flush:		push	eax
+		push	ebx
+		push	edx
+		mov	eax, 4		; sys_write
+		mov	ebx, 1		; stdout
+		mov	ecx, output_buf
+		mov	edx, LEN_OUTPUT_BUF
+		int	0x80
+		pop	edx
+		pop	ebx
+		pop	eax
+		mov	ecx, LEN_OUTPUT_BUF	
+		mov	edi, output_buf
+.done:		ret
